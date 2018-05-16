b9f8d9139d00bd0522f318a0179b1eefccc51aef
Gabriel Rohden <gabriel_rohden@outlook.com>
2018-05-16 13:30:02 -0300

    Find non friends query fix (bad one, but i'm in a hurry)

--- src/main/java/social/repositories/PersonRepository.java
+++ src/main/java/social/repositories/PersonRepository.java
@@ -16,7 +16,7 @@ public interface PersonRepository extends CrudRepository<Person, Long> {
     @Query("SELECT DISTINCT p FROM Person p JOIN Friendship f on (:personid IN (f.friend.id, f.friendship.id) AND p.id IN (f.friend.id, f.friendship.id)) WHERE p.id <> :personid")
     List<Person> findFriends(@Param("personid") Long personId);
 
-    @Query("SELECT p FROM Person p WHERE p.id NOT IN (SELECT DISTINCT p.id FROM Person p JOIN Friendship f on (:personid IN (f.friend.id, f.friendship.id) AND p.id IN (f.friend.id, f.friendship.id)))")
+    @Query("SELECT p FROM Person p WHERE p.id NOT IN (SELECT DISTINCT p.id FROM Person p JOIN Friendship f on (:personid IN (f.friend.id, f.friendship.id) AND p.id IN (f.friend.id, f.friendship.id))) AND p.id <> :personid")
     List<Person> findNonFriends(@Param("personid") Long personId);
 
     Person findPersonById(@Param("personid") Long personId);
